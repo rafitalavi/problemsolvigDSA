bb8eca7628740a8f1fc9f4e945c6086703f8702f
rafitalavi <128486927+rafitalavi@users.noreply.github.com>
2023-10-08 08:52:09 +0600

    half diamond pattern
    output:
    *
    **
    ***
    ****
    *****
    ******
    ******
    *****
    ****
    ***
    **
    *

---
+++ horaizontalpattern.py
@@ -0,0 +1,14 @@
+t = int(input())
+for x in range (t+1):
+  
+    for y in range (x):
+        print("*",end="")
+
+    print()
+
+for x in range (t-1,0,-1):
+  
+    for y in range (x):
+        print("*",end="")
+
+    print()        
